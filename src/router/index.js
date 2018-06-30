7f689f5efd925829509f082a67baf917c954772f
lnh <609556206@qq.com>
2018-06-30 12:07:14 +0800

    添加接口注释

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ const client = require('../controller/client')
 
 module.exports = {
     async server (app) {
+        // 后台接口列表
         router.get('/server', server.test)
     
         app.use(router.routes())
     },
     async client (app) {
+        // 客户端接口列表
         router.get('/client', client.test)
     
         app.use(router.routes())
