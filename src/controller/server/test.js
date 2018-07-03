e41969e3cd45399857cdea8883e51f2089031b3c
lnh <scjlnh@163.com>
2018-07-03 17:44:23 +0800

    加入mongodb

--- src/controller/server/test.js
+++ src/controller/server/test.js
@@ -1,6 +1,6 @@
 module.exports = async (ctx, next) => {
     ctx.response.body = {
-        type: 'get',
+        type: 'post',
         msg: '请求成功，优化业务逻辑后的信息-->server',
         parser: ctx.request.body
     }
