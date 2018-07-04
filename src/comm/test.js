846498452955a135479315315dc98c274c88e437
lnh <scjlnh@163.com>
2018-07-04 18:05:54 +0800

    添加数据

--- src/comm/test.js
+++ src/comm/test.js
@@ -4,9 +4,21 @@ const url = 'mongodb://localhost:27017';
 MongoClient.connect(url, (err, db) => {
     if (err) throw err;
     const dbLnh = db.db('lnh');
-    dbLnh.collection('site').find().toArray((err, result) => {
+    const data = [
+        {"name":"lnh3", "sex":"女", "age": "16"},
+        {"name":"lnh4", "sex":"女", "age": "17"},
+        {"name":"lnh5", "sex":"男", "age": "18"}
+    ]
+    dbLnh.collection('site').insertMany(data, (err, result) => {
         if(err) throw err;
+        console.log('插入文档成功');
         console.log(result);
         db.close();
+
+        // dbLnh.collection('site').find().toArray((err, result) => {
+        //     if(err) throw err;
+        //     console.log(result);
+        //     db.close();
+        // })
     })
 })
