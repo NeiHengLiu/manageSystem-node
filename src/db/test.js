2f866efe86631a2e79885d005bef98f80fdf2cd0
lnh <scjlnh@163.com>
2018-07-05 17:56:39 +0800

    修改数据库的连接

--- src/db/test.js
+++ src/db/test.js
@@ -3,7 +3,7 @@ const url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, (err, db) => {
     if (err) throw err;
-    console.log(db)
+    // console.log(db)
     const dbLnh = db.db('lnh');
     const data = [
         {"name":"赵园园", "sex":"女", "age": "18", "professional": "演员"},
@@ -11,7 +11,7 @@ MongoClient.connect(url, (err, db) => {
         {"name":"李小龙", "sex":"男", "age": "35", "professional": "演员"},
         {"name":"马化云", "sex":"男", "age": "42", "professional": "企业家"},
         {"name":"马腾", "sex":"男", "age": "54", "professional": "企业家"}
-    ]
+    ];
     dbLnh.collection('site').insertMany(data, (err, result) => {
         if(err) throw err;
         console.log('插入文档成功');
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,31 @@ const router = require('koa-router')()
 const server = require('../controller/server')
 const client = require('../controller/client')
 
+const serverAPI = '/server'
+const clientAPI = '/client'
+
 module.exports = {
-    async server (app) {
-        // 后台接口列表
-        router.post('/server', server.test)
+    async server (app) {    // 后台接口列表
+        /**
+         * 测试接口
+         */
+
+        router.post(serverAPI, server.test)
+
+        /**
+         * 登录
+         */
+
+        router.post(serverAPI + '/login/login', server.test)
     
         app.use(router.routes())
     },
-    async client (app) {
-        // 客户端接口列表
-        router.get('/client', client.test)
+    async client (app) {    // 客户端接口列表
+        /**
+         * 测试接口
+         */
+        
+        router.get(clientAPI, client.test)
     
         app.use(router.routes())
     }
