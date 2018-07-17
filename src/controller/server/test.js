7ac798681dc4033f0a2a154a2245511c644c1165
lnh <scjlnh@163.com>
2018-07-17 18:02:19 +0800

    修改代码

--- src/controller/server/test.js
+++ src/controller/server/test.js
@@ -1,7 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+
 module.exports = async (ctx, next) => {
-    ctx.response.body = {
-        type: 'post',
-        msg: '请求成功，优化业务逻辑后的信息-->server',
-        parser: ctx.request.body
-    }
+    let tempStr = [];
+    await MongoClient.connect(url, (err, db) => {
+        if(err) throw err;
+        let dbo = db.db('lnh');
+        dbo.collection('site').find({}).toArray((err, result) => {
+            if(err) throw err;
+            // tempStr = result;
+            tempStr = result;
+            console.log(result);
+            db.close();
+        })
+    })
+    ctx.response.body = tempStr;
+    // ctx.response.body = tempStr;
+    // ctx.response.body = {
+    //     type: 'post',
+    //     msg: '请求成功，优化业务逻辑后的信息-->server',
+    //     parser: ctx.request.body
+    // }
 }
