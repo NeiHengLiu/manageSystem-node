c4adf4acba2e8c31e3ac41b043e3d6102d657ce3
lnh <scjlnh@163.com>
2018-07-18 17:54:09 +0800

    引入mongoose

--- src/controller/server/test.js
+++ src/controller/server/test.js
@@ -1,24 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://127.0.0.1:27017';
+const user = require('../../db').get('user')
 
 module.exports = async (ctx, next) => {
-    let tempStr = [];
-    await MongoClient.connect(url, (err, db) => {
-        if(err) throw err;
-        let dbo = db.db('lnh');
-        dbo.collection('site').find({}).toArray((err, result) => {
-            if(err) throw err;
-            // tempStr = result;
-            tempStr = result;
-            console.log(result);
-            db.close();
-        })
-    })
-    ctx.response.body = tempStr;
-    // ctx.response.body = tempStr;
-    // ctx.response.body = {
-    //     type: 'post',
-    //     msg: '请求成功，优化业务逻辑后的信息-->server',
-    //     parser: ctx.request.body
-    // }
+    // let tempVal = await user.find({})
+    let tempVal = await user.remove({userName: 'lnh'})
+    ctx.response.body = tempVal
 }
+
--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,3 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://127.0.0.1:27017';
+const db = require('monk')('127.0.0.1:27017/manageSystemDB');
 
+module.exports = db
